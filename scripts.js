7689b4cb300dcaeffb56e73c88b25df01295bd7b
Gintautas Poderys <gintautasp25@gmail.com>
2023-09-04 18:00:46 +0300

    Created calculator operator functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log('Scripts working')
+function add(var1, var2){
+    return +var1 + +var2
+}
+function subtract(var1, var2){
+    return var1 - var2
+
+}function multiply(var1, var2){
+    return var1 * var2
+
+}function divide(var1, var2){
+    if (var2 === 0) return 'error'
+    return var1 / var2
+}
+
+let var1 = 0, var2 = 0, operator = 0
+
+function operate(var1, var2, operator){
+    switch(operator){
+        case add:
+            add(var1, var2)
+        case subtract:
+            subtract(var1, var2)
+    }
+}
